78530eba5472955ba36db17474ef212b01543385
sabiduria <antdepadoue@gmail.com>
2018-08-09 11:21:00 +0200

    Implementation of Tooltips Engine

---
+++ Tooltip-Engine/Engine.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Antoine.BARUANI
+ * Date: 09/08/2018
+ * Time: 09:37
+ */
+class Engine
+{
+    private $type;
+    private $text;
+
+    /**
+     * Engine constructor.
+     * @param $type
+     * @param $text
+     */
+    public function __construct($type, $text)
+    {
+        $this->type = $type;
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    function render_tooltips()
+    {
+        $tooltip = "";
+        $begin_tooltip   = '[';
+        $end_tooltip   = ']';
+        $begin_at_position = strpos($this->getText(), $begin_tooltip);
+        $end_at_position = strpos($this->getText(), $end_tooltip);
+
+        if ($this->getType()){
+            $extracted_text = substr($this->getText(), $begin_at_position, ($end_at_position-$begin_at_position)+1);
+            $json_extracted_text = json_decode($extracted_text, true);
+            foreach ($json_extracted_text as $key=>$value){
+                $tooltip = '<a class="mytooltip" href="javascript:void(0)">'.$value["key"].'<span class="tooltip-content5"><span class="tooltip-text3"><span class="tooltip-inner2">'.$value["text"].'</span>';
+            }
+            return str_replace($extracted_text, $tooltip, $this->getText());
+        }
+        return "";
+    }
+}
